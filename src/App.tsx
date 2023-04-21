fe60d7a6b1f4cd856518becca3af7e6246ace5b8
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2023-04-22 00:27:07 +0200

    Add Chakra grid and remove css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Grid, GridItem } from '@chakra-ui/react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <Grid
+      templateAreas={`"header" "main" "footer"`}
+      gridTemplateRows={'50px 1fr 30px'}
+      gridTemplateColumns={'1fr'}
+      h='100vh'
+      color='#ffffff'
+      fontWeight='bold'
+    >
+      <GridItem pl='2' bg='#200d19' area={'header'}>
+        CS:GO Game Match Stats
+      </GridItem>
+      <GridItem pl='2' bg='#200d19' area={'main'}>
+        Main
+      </GridItem>
+      <GridItem pl='2' bg='#251821' area={'footer'}>
+        @{' '}
+        <a href='https://github.com/MTraveller' target='_blank'>
+          MTraveller
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
