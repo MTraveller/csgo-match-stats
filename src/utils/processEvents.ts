68b298df0c80d0ffdbe3aa0c203abf994610601a
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2023-04-25 00:06:00 +0200

    Process playing map and teams

--- src/utils/processEvents.ts
+++ src/utils/processEvents.ts
@@ -1,3 +1,4 @@
+import processMapTeams from './processMapTeams';
 import processPlayers from './processPlayers';
 
 const enum Event {
@@ -15,11 +16,12 @@ function processEvents(matchLog: string[]) {
   const matchStart = matchLog.slice(
     matchLog.findLastIndex(e => e.includes(Event.start))
   );
-
   const players = processPlayers(matchLog, Event.assigned);
+  const playingTeams = processMapTeams(matchStart);
 
+  console.log(playingTeams);
   console.log(players);
-  console.log(matchStart);
+  console.log(matchStart[1]);
 
   return [1, 2, 3];
 }
