5fffe04328490395e5fda7fae399b27ab023f787
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2023-04-29 01:43:35 +0200

    Fix responsiveness of the round picker + add styles to text

--- src/components/MatchResult/TopCT.tsx
+++ src/components/MatchResult/TopCT.tsx
@@ -7,17 +7,36 @@ interface TopPlayers {
 function TopCT({ topCTPlayer }: TopPlayers) {
   return (
     <Box w='full' display='flex' flexDir='column' alignItems='center' gap={3}>
-      <Heading size='md'>Top Counter Terrorist Player:</Heading>
+      <Heading size='md' color='whiteAlpha.700'>
+        Top Counter Terrorist Player:
+      </Heading>
       <Box w='full' display='flex' flexDir='column' alignItems='center' gap={2}>
-        <Heading size='2xl'>{topCTPlayer[0]}</Heading>
+        <Heading
+          size='2xl'
+          fontWeight='black'
+          color='whiteAlpha.700'
+          bgGradient='linear(to-r, #000000, blue.500, #000000)'
+          px={16}
+          py={2}
+        >
+          {topCTPlayer[0]}
+        </Heading>
         <Box w='full'>
           <Box w='full' display='flex' justifyContent='space-around'>
-            <Text fontSize='2xl'>Damage done:</Text>
-            <Text fontSize='2xl'>{topCTPlayer[1]}</Text>
+            <Text fontSize='2xl' color='whiteAlpha.700' p={2}>
+              Damage done:
+            </Text>
+            <Text fontSize='2xl' color='whiteAlpha.700'>
+              {topCTPlayer[1]}
+            </Text>
           </Box>
           <Box w='full' display='flex' justifyContent='space-around'>
-            <Text fontSize='2xl'>Kills done:</Text>
-            <Text fontSize='2xl'>{topCTPlayer[2]}</Text>
+            <Text fontSize='2xl' color='whiteAlpha.700'>
+              Kills done:
+            </Text>
+            <Text fontSize='2xl' color='whiteAlpha.700'>
+              {topCTPlayer[2]}
+            </Text>
           </Box>
         </Box>
       </Box>
--- src/components/MatchResult/TopTR.tsx
+++ src/components/MatchResult/TopTR.tsx
@@ -7,17 +7,36 @@ interface TopPlayers {
 function TopTR({ topTRPlayer }: TopPlayers) {
   return (
     <Box w='full' display='flex' flexDir='column' alignItems='center' gap={3}>
-      <Heading size='md'>Top Terrorist Player:</Heading>
+      <Heading size='md' color='whiteAlpha.700'>
+        Top Counter Terrorist Player:
+      </Heading>
       <Box w='full' display='flex' flexDir='column' alignItems='center' gap={2}>
-        <Heading size='2xl'>{topTRPlayer[0]}</Heading>
+        <Heading
+          size='2xl'
+          fontWeight='black'
+          color='whiteAlpha.700'
+          bgGradient='linear(to-r, #000000, red.500, #000000)'
+          px={16}
+          py={2}
+        >
+          {topTRPlayer[0]}
+        </Heading>
         <Box w='full'>
           <Box w='full' display='flex' justifyContent='space-around'>
-            <Text fontSize='2xl'>Damage done:</Text>
-            <Text fontSize='2xl'>{topTRPlayer[1]}</Text>
+            <Text fontSize='2xl' color='whiteAlpha.700' p={2}>
+              Damage done:
+            </Text>
+            <Text fontSize='2xl' color='whiteAlpha.700'>
+              {topTRPlayer[1]}
+            </Text>
           </Box>
           <Box w='full' display='flex' justifyContent='space-around'>
-            <Text fontSize='2xl'>Kills done:</Text>
-            <Text fontSize='2xl'>{topTRPlayer[2]}</Text>
+            <Text fontSize='2xl' color='whiteAlpha.700'>
+              Kills done:
+            </Text>
+            <Text fontSize='2xl' color='whiteAlpha.700'>
+              {topTRPlayer[2]}
+            </Text>
           </Box>
         </Box>
       </Box>
