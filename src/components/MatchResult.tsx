3be05ccb630e1cbc21aafe669131f97223ac3fe9
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2023-04-30 19:08:23 +0200

    Fix vercel not found module error

--- src/components/MatchResult.tsx
+++ src/components/MatchResult.tsx
@@ -4,7 +4,7 @@ import EventsContext from '../contexts/eventsContexts';
 import TopPlayersContext from '../contexts/topPlayersContext';
 import useRoundStore from '../stores/roundStore';
 import processTopPlayers from '../utils/processTopPlayers';
-import { XL } from './matchresult/layout';
+import XL from './matchresult/layout/XL';
 
 function MatchResult() {
   const breakpoint = useBreakpoint({ ssr: false });
