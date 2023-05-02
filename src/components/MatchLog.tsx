ce3e9397cd49e0fdb026a0f724455970df29b5b3
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2023-05-02 22:57:13 +0200

    Clean up

--- src/components/MatchLog.tsx
+++ src/components/MatchLog.tsx
@@ -5,7 +5,6 @@ import EventsContext, {
 } from '../contexts/eventsContexts';
 import useProcessLog from '../hooks/useProcessLog';
 import useMatchLengthStore from '../stores/lengthStore';
-import useUrlLogsStore from '../stores/urlLogsStore';
 import MatchResult from './MatchResult';
 
 interface ProcessLogs {
@@ -19,7 +18,6 @@ function MatchLog() {
   const { statuses, roundsPlayersStats, isError, error }: ProcessLogs =
     useProcessLog();
   const { matchLength, setMatchLength } = useMatchLengthStore();
-  const { isLog } = useUrlLogsStore();
 
   useEffect(() => {
     const statusesLength = Object.keys(statuses).length;
@@ -28,11 +26,11 @@ function MatchLog() {
 
   return isError ? (
     <h2>{error?.message}</h2>
-  ) : isLog ? (
+  ) : (
     <EventsContext.Provider value={{ statuses, roundsPlayersStats }}>
       <MatchResult />
     </EventsContext.Provider>
-  ) : <h2>Error: The entered URL has no match logs!!!</h2>;
+  );
 }
 
 export default MatchLog;
--- src/hooks/useProcessLog.ts
+++ src/hooks/useProcessLog.ts
@@ -1,21 +1,14 @@
 import processEvents from '../utils/processEvents';
 import useLogFetcher from './useLogFetcher';
-import useUrlLogsStore from '../stores/urlLogsStore';
 
 const useProcessLog = () => {
   const { data, isError, error } = useLogFetcher();
-  const { setLog } = useUrlLogsStore();
 
   const gameEvents: string[] = [];
 
-  try {
-    gameEvents.push(...JSON.stringify(data).split('\\r\\n'));
-  } catch {
-    setLog(false);
-  }
+  if (data) gameEvents.push(...JSON.stringify(data).split('\\r\\n'));
 
   const { statuses, roundsPlayersStats } = processEvents(gameEvents);
-
   return { statuses, roundsPlayersStats, isError, error };
 };
 
